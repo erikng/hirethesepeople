04ef9409038f115b9f63e30f260ca7710b69722c
Austin Culter <austin.culter@uber.com>
2020-05-20 11:36:00 +0200

    Add contact URL validation

--- generate_employee.sh
+++ generate_employee.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+validate_contact_urls () {
+  if [[ $linkedin == "" && $github == "" && $homepage == "" ]]; then
+    echo "You must provide at least one 'Contact' URL."
+    exit 1
+  fi
+}
+
 date=$(date '+%Y-%m-%d')
 
 echo -e "This is a (crude) helper script to easily generate a new employee page on the site. Please fill in the fields with your data and a new markdown page will be generated for you.\n"
@@ -24,11 +32,17 @@ read about
 echo "Experience/skills:"
 read experience
 
-echo "LinkedIn URL:"
+echo "You must provide at least one of the following three 'Contact' URL's:"
+echo "LinkedIn URL (leave blank if not applicable):"
 read linkedin
 
-echo "GitHub URL:"
+echo "GitHub URL (leave blank if not applicable):"
 read github
+
+echo "Homepage URL(leave blank if not applicable):"
+read homepage
+
+validate_contact_urls
 
 echo -e "\n\n"
 echo "Please review the following:"
@@ -40,6 +54,7 @@ echo "About: $about"
 echo "Experience/skills: $experience"
 echo "LinkedIn URL: $linkedin"
 echo "GitHub URL: $github"
+echo "Homepage URL: $homepage"
 echo -e "\nIs this correct (y/n)?"
 read correct
 
@@ -66,11 +81,19 @@ if [[ $correct == "y" ]]; then
   echo "${experience}" >> $employee_page
   echo "" >> $employee_page
   echo "## Contact Me" >> $employee_page
-  echo "[LinkedIn](${linkedin})" >> $employee_page
-  echo "[GitHub](${github})" >> $employee_page
+  if [[ $linkedin != "" ]]; then
+    echo "* [LinkedIn](${linkedin})" >> $employee_page
+  fi
+  if [[ $github != "" ]]; then
+    echo "* [GitHub](${github})" >> $employee_page
+  fi
+  if [[ $homepage != "" ]]; then
+    echo "* [Homepage](${homepage})" >> $employee_page
+  fi
   echo "" >> $employee_page
 
   echo "Employee markdown page has been generated at ./content/employees/${first_lower}_${last_lower}.md"
 else
   echo "Something went wrong; try again."
+  exit 2
 fi
